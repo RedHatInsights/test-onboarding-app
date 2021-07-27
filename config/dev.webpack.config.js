a75ea7f5001156d6d22c01a6a141cfba43b28be1
Jason Stephens <jassteph@redhat.com>
2021-07-27 13:56:47 -0400

    updating app name

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -8,7 +8,7 @@ const { config: webpackConfig, plugins } = config({
   deployment: process.env.BETA ? 'beta/apps' : 'apps',
   useProxy: true,
   useCloud: true, // Until console.redhat.com is working
-  appUrl: process.env.BETA ? '/beta/staging/starter' : '/staging/starter',
+  appUrl: process.env.BETA ? '/beta/staging/test-onboarding-app' : '/staging/test-onboarding-app',
   env: process.env.BETA ? 'ci-beta' : 'ci-stable',
   standalone: Boolean(process.env.STANDALONE),
 });
